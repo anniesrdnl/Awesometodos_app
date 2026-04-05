963dcf09246a3a3471a5f168667c6add7b9b6510
anniesrdnl <anniesardiniola@gmail.com>
2026-04-05 11:24:49 +0800

    Updated again for index code

--- server/models/index.js
+++ server/models/index.js
@@ -1,35 +1,4 @@
-<<<<<<< HEAD
 const { MongoClient } = require("mongodb");
-=======
-/*
-const { MongoClient } = require("mongodb")
-
-const uri = process.env.MONGODB_URI
-const client = new MongoClient(uri)
-
-let database
-
-async function connectDB() {
-    await client.connect()
-    database = client.db("myDatabase") // change if needed
-    console.log("MongoDB Connected")
-}
-
-function getCollection(name) {
-    if (!database) {
-        throw new Error("Database not initialized")
-    }
-    return database.collection(name)
-}
-
-module.exports = {
-    connectDB,
-    getCollection
-}
-*/
-const { MongoClient } = require("mongodb");
-// Add this line to load the .env file
->>>>>>> 1cfff29ba42e175413ffeae4bfc1dc95726e76ae
 require("dotenv").config();
 
 const uri = process.env.MONGODB_URI;
@@ -40,10 +9,6 @@ let database;
 async function connectDB() {
     try {
         await client.connect();
-<<<<<<< HEAD
-=======
-        // Make sure "myDatabase" matches your MongoDB Atlas setup
->>>>>>> 1cfff29ba42e175413ffeae4bfc1dc95726e76ae
         database = client.db("myDatabase");
         console.log("MongoDB Connected");
     } catch (err) {
